3f3c42a01bb74a8d6c747469eefd8ddcdac61e2e
Isamar Banos <Isamargonzalez90@gmail.com>
2023-10-24 14:47:50 -0400

    completed partnership assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+function Item({ name, category }) {   
+  const [itemInCart, setItemInCart] = useState(false); 
 
-function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
-}
+  function UpdateItemInCart() {   
+    setItemInCart((itemInCart) => !itemInCart);   
+    console.log(itemInCart)  }  
+    
+    const isItemInCart = itemInCart ? "in-cart" : ""    
+
+    
+    return (<li className={isItemInCart}>      
+     <span>{name}</span>       
+     <span className="category">{category}</span>      
+      <button className={itemInCart ? "remove" : "add"} onClick={UpdateItemInCart} >{itemInCart ? "Remove from cart" : "Add to cart"} </button>   
+        </li>   
+        );
+       }  
+
 
 export default Item;
+     
